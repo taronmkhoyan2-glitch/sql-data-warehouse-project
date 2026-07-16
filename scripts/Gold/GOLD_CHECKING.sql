420f3afb5f71ec48308ed81bc4eed5ef2786c67f
taronmkhoyan2-glitch <taronmkhoyan2@gmail.com>
2026-07-17 01:11:30 +0400

    Update GOLD_CHECKING.sql

--- scripts/Gold/GOLD_CHECKING.sql
+++ scripts/Gold/GOLD_CHECKING.sql
@@ -5,7 +5,7 @@ select distinct
  ci.cst_gndr,
  
  ca.gen,
-case when ci.cst_gndr!='n/a'  then ci.cst_gndr       
+case when ci.cst_gndr!='n/a'  then ci.cst_gndr        
       else coalesce(gen,'n/a') 
 end as new_gen
  
@@ -22,3 +22,41 @@ select
 *
 from
 gold.dim_customers
+
+------------Cheking the uniqunes in Product dimenshon
+
+
+Select prd_key, count(*) from (
+
+select
+pn.prd_id,
+pn.prd_key,
+pn.prd_nm,
+pn.cat_id,
+pc.cat,
+pc.subcat,
+pc.maintenance,
+pn.prd_cost,
+pn.prd_line,
+pn.prd_start_dt
+
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id=pc.id
+
+where prd_end_dt is null--Filter out historical data
+) t group by prd_key
+having count(*) >1 
+
+
+-----Checking the data issues based on Joining the views
+
+select * from gold.fact_sales f
+left join gold.dim_customers c
+on c.customer_key=f.customer_key
+left join gold.dim_products p
+on p.product_key=f.product_key
+where c.customer_key is null 
+or p.product_key is null
+
+----if we get the result it means we have no match
